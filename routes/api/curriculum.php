bef7a914d7de43faa62f75e74f0f87b31ebd40d4
husnilk <husnil.k@gmail.com>
2023-01-03 22:52:29 +0700

    Remove unnecessary auth components and clean routing

---
+++ routes/api/curriculum.php
@@ -0,0 +1,50 @@
+<?php
+
+//List Rps
+use App\Http\Controllers\Api\Curriculum\CourseLoController;
+use App\Http\Controllers\Api\Curriculum\CoursePlanAssessmentController;
+use App\Http\Controllers\Api\Curriculum\CoursePlanController;
+use App\Http\Controllers\Api\Curriculum\CoursePlanDetailController;
+use App\Http\Controllers\Api\Curriculum\LecturerController;
+use App\Http\Controllers\Api\Curriculum\ListCoursePlanController;
+use App\Http\Controllers\Api\Curriculum\RefController;
+
+Route::get('/rps', [ListCoursePlanController::class, 'index']);
+Route::post('/rps', [ListCoursePlanController::class, 'search']);
+Route::get('/rps/{rpsId}', [ListCoursePlanController::class, 'show']);
+Route::post('/rps/{rpsId}/export', [ListCoursePlanController::class, 'export']);
+
+//Rps
+Route::get('/bo/rps/', [CoursePlanController::class, 'index']);
+Route::post('/bo/rps/', [CoursePlanController::class, 'store']);
+Route::get('/bo/rps/{rpsId}/', [CoursePlanController::class, 'show']);
+Route::put('/bo/rps/{rpsId}/', [CoursePlanController::class, 'update']);
+
+//CPMK
+Route::get('/bo/rps/{rpsId}/cpmk', [CourseLoController::class, 'getData']);
+Route::post('/bo/rps/{rpsId}/cpmk', [CourseLoController::class, 'store']);
+Route::put('/bo/rps/{rpsId}/cpmk/{cpmkId}', [CourseLoController::class, 'update']);
+Route::delete('/bo/rps/{rpsId}/cpmk/{cpmkId}', [CourseLoController::class, 'destroy']);
+
+//Ref
+Route::get('/bo/rps/{rpsId}/refs', [RefController::class, 'index']);
+Route::post('/bo/rps/{rpsId}/refs', [RefController::class, 'store']);
+Route::put('/bo/rps/{rpsId}/refs/{refId}', [RefController::class, 'update']);
+Route::delete('/bo/rps/{rpsId}/refs/{refId}', [RefController::class, 'destroy']);
+
+//Assessment
+Route::get('/bo/rps/{rpsId}/assessments', [CoursePlanAssessmentController::class, 'index']);
+Route::post('/bo/rps/{rpsId}/assessments', [CoursePlanAssessmentController::class, 'store']);
+Route::put('/bo/rps/{rpsId}/assessments/{assessmentId}', [CoursePlanAssessmentController::class, 'update']);
+Route::delete('/bo/rps/{rpsId}/assessments/{assessmentId}', [CoursePlanAssessmentController::class, 'destroy']);
+
+//Session
+Route::get('/bo/rps/{rpsId}/session', [CoursePlanDetailController::class, 'index']);
+Route::post('/bo/rps/{rpsId}/session', [CoursePlanDetailController::class, 'store']);
+Route::put('/bo/rps/{rpsId}/session/{sessionId}', [CoursePlanDetailController::class, 'update']);
+Route::delete('/bo/rps/{rpsId}/session/{sessionId}', [CoursePlanDetailController::class, 'destroy']);
+
+//Lecturer
+Route::get('/bo/rps/{rpsId}/lecturers', [LecturerController::class, 'index']);
+Route::post('/bo/rps/{rpsId}/lecturers', [LecturerController::class, 'store']);
+Route::delete('/bo/rps/{rpsId}/lecturers/{lecturersId}', [LecturerController::class, 'destroy']);
--- routes/api/frontend/thesis.php
+++ routes/api/frontend/thesis.php
@@ -27,54 +27,53 @@ use App\Http\Controllers\Api\Thesis\ThesisTrialController;
 use App\Http\Controllers\Api\Thesis\ThesisTrialSubmissionController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['api', 'auth']], function ($router) {
+
+/** MAHASISWA */
+//Route::get('students/{id}', [ThesisController::class, 'showmahasiswa']);
+//Route::get('/list/seminars', [ThesisListController::class, 'seminars']);
+//Route::get('/list/trials', [ThesisListController::class, 'trials']);
+//
+//Route::resource('theses.submissions', ThesisSubmissionController::class)->except(['edit', 'create']);
+//Route::resource('theses', ThesisController::class)->except(['edit', 'create']);
+//Route::resource('theses.proposals', ThesisProposalController::class);
+//Route::resource('theses.logbooks', ThesisLogbookController::class)->except(['edit', 'create']);
+//Route::resource('theses.seminars', ThesisSeminarController::class)->except(['edit', 'create']);
+//Route::resource('theses.trials', ThesisTrialController::class)->only(['show', 'store', 'index']);
+
+Route::group(['prefix' => 'thesis', 'as' => 'thesis.'], function () {
     /** MAHASISWA */
-    Route::get('students/{id}', [ThesisController::class, 'showmahasiswa']);
-    Route::get('/list/seminars', [ThesisListController::class, 'seminars']);
-    Route::get('/list/trials', [ThesisListController::class, 'trials']);
+//    Route::resource('seminars.audiences', ThesisSeminarAudienceController::class)->only(['create', 'store', 'destroy', 'index']);
 
-    Route::resource('theses.submissions', ThesisSubmissionController::class)->except(['edit', 'create']);
-    Route::resource('theses', ThesisController::class)->except(['edit', 'create']);
-    Route::resource('theses.proposals', ThesisProposalController::class);
-    Route::resource('theses.logbooks', ThesisLogbookController::class)->except(['edit', 'create']);
-    Route::resource('theses.seminars', ThesisSeminarController::class)->except(['edit', 'create']);
-    Route::resource('theses.trials', ThesisTrialController::class)->only(['show', 'store', 'index']);
+    /** DOSEN */
+//    Route::resource('advisors', ThesisAdvisorController::class)->except(['create', 'store']); //Tadi diubah
+//    Route::resource('submissions', ThesisSupervisorSubmissionController::class);
+//    Route::post('submissions/{thesis}/accept', [ThesisSupervisorSubmissionController::class, 'accept'])->name('submissions.accept');
+//    Route::post('submissions/{thesis}/reject', [ThesisSupervisorSubmissionController::class, 'reject'])->name('submissions.reject');
+//    Route::resource('advisor-cancellation', ThesisAdvisorCancellationController::class)->only(['create', 'update']);
+//    Route::resource('advisors.logbooks', ThesisAdvisorLogbookController::class)->only(['show', 'edit', 'update']);
+//    Route::post('advisors-logbooks/{thesis}/accept', [ThesisAdvisorLogbookController::class, 'accept'])->name('advisors.logbook.accept');
+//    Route::post('advisors-logbooks/{thesis}/reject', [ThesisAdvisorLogbookController::class, 'reject'])->name('advisors.logbook.reject');
+//
+//    Route::resource('proposal-grades', ThesisProposalGradeController::class)->only(['index', 'show', 'edit', 'update']);
+//
+//    Route::resource('advisors.seminars', ThesisSupervisorSeminarController::class);
+//    Route::resource('reviewer-submissions', ThesisReviewerSubmissionController::class)->only(['index', 'show', 'update', 'destroy']);
+//    Route::resource('reviewer-scores', ThesisReviewerScoreController::class)->only(['edit', 'update']);
+//
+//    Route::resource('advisors.trials', ThesisSupervisorTrialController::class);
+//    Route::resource('examiner-submissions', ThesisExaminerSubmissionController::class)->only(['index', 'show', 'update', 'destroy']);
+//    Route::resource('examiner-scores', ThesisExaminerScoreController::class)->only(['edit', 'update']);
+//
+//    Route::resource('grades', ThesisAdvisorGradeController::class)->except(['update', 'destroy', 'create', 'store']);
 
-    Route::group(['prefix' => 'thesis', 'as' => 'thesis.'], function () {
-        /** MAHASISWA */
-        Route::resource('seminars.audiences', ThesisSeminarAudienceController::class)->only(['create', 'store', 'destroy', 'index']);
-
-        /** DOSEN */
-        Route::resource('advisors', ThesisAdvisorController::class)->except(['create', 'store']); //Tadi diubah
-        Route::resource('submissions', ThesisSupervisorSubmissionController::class);
-        Route::post('submissions/{thesis}/accept', [ThesisSupervisorSubmissionController::class, 'accept'])->name('submissions.accept');
-        Route::post('submissions/{thesis}/reject', [ThesisSupervisorSubmissionController::class, 'reject'])->name('submissions.reject');
-        Route::resource('advisor-cancellation', ThesisAdvisorCancellationController::class)->only(['create', 'update']);
-        Route::resource('advisors.logbooks', ThesisAdvisorLogbookController::class)->only(['show', 'edit', 'update']);
-        Route::post('advisors-logbooks/{thesis}/accept', [ThesisAdvisorLogbookController::class, 'accept'])->name('advisors.logbook.accept');
-        Route::post('advisors-logbooks/{thesis}/reject', [ThesisAdvisorLogbookController::class, 'reject'])->name('advisors.logbook.reject');
-
-        Route::resource('proposal-grades', ThesisProposalGradeController::class)->only(['index', 'show', 'edit', 'update']);
-
-        Route::resource('advisors.seminars', ThesisSupervisorSeminarController::class);
-        Route::resource('reviewer-submissions', ThesisReviewerSubmissionController::class)->only(['index', 'show', 'update', 'destroy']);
-        Route::resource('reviewer-scores', ThesisReviewerScoreController::class)->only(['edit', 'update']);
-
-        Route::resource('advisors.trials', ThesisSupervisorTrialController::class);
-        Route::resource('examiner-submissions', ThesisExaminerSubmissionController::class)->only(['index', 'show', 'update', 'destroy']);
-        Route::resource('examiner-scores', ThesisExaminerScoreController::class)->only(['edit', 'update']);
-
-        Route::resource('grades', ThesisAdvisorGradeController::class)->except(['update', 'destroy', 'create', 'store']);
-
-        /** KAPRODI */
-        Route::resource('seminar-submissions', ThesisSeminarSubmissionController::class)->only(['index', 'show', 'update', 'destroy']);
-        Route::resource('seminar.reviewer-assignments', ThesisSeminarReviewerAssignmentController::class)->only(['create', 'store', 'destroy']);
-
-        Route::resource('trial-submissions', ThesisTrialSubmissionController::class)->only(['index', 'show', 'update', 'destroy']);
-        Route::resource('trials.examiner-assignments', ThesisTrialExaminerAssignmentController::class)->only(['create', 'store', 'destroy']);
-
-        Route::resource('finals', ThesisFinalController::class)->only(['index', 'show', 'update']);
-
-    });
+    /** KAPRODI */
+//    Route::resource('seminar-submissions', ThesisSeminarSubmissionController::class)->only(['index', 'show', 'update', 'destroy']);
+//    Route::resource('seminar.reviewer-assignments', ThesisSeminarReviewerAssignmentController::class)->only(['create', 'store', 'destroy']);
+//
+//    Route::resource('trial-submissions', ThesisTrialSubmissionController::class)->only(['index', 'show', 'update', 'destroy']);
+//    Route::resource('trials.examiner-assignments', ThesisTrialExaminerAssignmentController::class)->only(['create', 'store', 'destroy']);
+//
+//    Route::resource('finals', ThesisFinalController::class)->only(['index', 'show', 'update']);
 
 });
+
