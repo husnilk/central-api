cdc00f6027b05d3d20c9b7757eab0bf904ce773b
husnilk <husnil.k@gmail.com>
2023-01-06 07:57:02 +0700

    Persiapan basis data untuk modul kerja praktek

--- database/migrations/2021_01_02_224735_create_internship_agencies_table.php
+++ database/migrations/2021_01_02_224735_create_internship_agencies_table.php
@@ -8,16 +8,18 @@ class CreateInternshipAgenciesTable extends Migration
 {
     public function up()
     {
-        Schema::create('internship_agencies', function (Blueprint $table) {
+        Schema::create('internship_companies', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('address');
+            $table->integer('status')->default(0);
+            $table->point('location')->nullable();
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('internship_agencies');
+        Schema::dropIfExists('internship_companies');
     }
 }
